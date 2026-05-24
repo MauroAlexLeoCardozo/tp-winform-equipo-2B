46c4c1e31ffc8c8f0c3d35ec75146e9dc737d02a
MauroAlexLeoCardozo <mauro.cardozo.dev@outlook.com>
2026-05-24 18:18:43 -0300

    Creacion de acceso a datos

---
+++ Negocio/accesoDatos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class accesoDatos
+    {
+        private SqlConnection conexion;
+        private sqlcommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public accesoDatos()
+        {
+            conexion = new SqlConnection("data source=DESKTOP-9K5QG8P\\SQLEXPRESS; initial catalog=CATALOGO_DB; integrated security=sspi");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+       public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+    }
+}
